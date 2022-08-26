f444ec5d1bdf32edd516d0c7846af0b6bdbda8b0
Satoru <you@example.com>
2022-08-26 12:27:08 +0900

    feat: add data

---
+++ pages/blog/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+   
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+    <!--
+    {{page}}
+    -->
+  </article>
+</template>
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    // console.log(`blog/${params.id}`)
+    const page = await $content(`blog/${params.id}`).fetch()
+    // console.log({page})
+    // const page = await $content(`blog`, `hello`).fetch()
+    return {
+      page
+    }
+  }
+}
+</script>
