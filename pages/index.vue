012c61b39a1c1ad437df93accebbc223489644d2
Satoru <you@example.com>
2022-08-26 12:12:57 +0900

    fix: bug

--- pages/index.vue
+++ pages/index.vue
@@ -83,5 +83,12 @@
 <script>
 export default {
   name: 'IndexPage',
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
+      ]
+    }
+  }
 }
 </script>
